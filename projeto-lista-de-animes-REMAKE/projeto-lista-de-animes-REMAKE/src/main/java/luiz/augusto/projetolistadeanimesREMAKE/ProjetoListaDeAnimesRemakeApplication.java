f8244c4d64b32cb94ca5693fcd1827cfbf5cf2de
Adela1de <94483883+Adela1de@users.noreply.github.com>
2022-08-12 10:08:34 -0300

    First commit

---
+++ projeto-lista-de-animes-REMAKE/projeto-lista-de-animes-REMAKE/src/main/java/luiz/augusto/projetolistadeanimesREMAKE/ProjetoListaDeAnimesRemakeApplication.java
@@ -0,0 +1,13 @@
+package luiz.augusto.projetolistadeanimesREMAKE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoListaDeAnimesRemakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoListaDeAnimesRemakeApplication.class, args);
+	}
+
+}
---
+++ projeto-lista-de-animes-REMAKE/projeto-lista-de-animes-REMAKE/src/test/java/luiz/augusto/projetolistadeanimesREMAKE/ProjetoListaDeAnimesRemakeApplicationTests.java
@@ -0,0 +1,13 @@
+package luiz.augusto.projetolistadeanimesREMAKE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoListaDeAnimesRemakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
