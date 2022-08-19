3e19c61fa2c64f2f5058700b65ee6bf628de8557
Adela1de <94483883+Adela1de@users.noreply.github.com>
2022-08-18 22:24:02 -0300

    Test heroku

--- projeto-lista-de-animes-REMAKE/projeto-lista-de-animes-REMAKE/src/main/java/luiz/augusto/projetolistadeanimesREMAKE/entities/AnimeRating.java
+++ projeto-lista-de-animes-REMAKE/projeto-lista-de-animes-REMAKE/src/main/java/luiz/augusto/projetolistadeanimesREMAKE/entities/AnimeRating.java
@@ -14,6 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 public class AnimeRating implements Serializable {
 
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
